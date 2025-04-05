adabb200b5035bbec5b6e287c438bb2d0d7b3133
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-05 15:23:40 -0400

    feat: basic BubbleTea viewer

--- cmd/gallery-cli/main.go
+++ cmd/gallery-cli/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"image/png"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	ascii "github.com/qeesung/image2ascii/convert"
 )
 
-func main() {
+func newApp() app {
 	f, err := os.Open("images/puppy1.png")
 	if err != nil {
 		panic(err)
@@ -18,5 +18,36 @@ func main() {
 		panic(err)
 	}
 	converter := ascii.NewImageConverter()
-	fmt.Printf("%s\n", converter.Image2ASCIIString(image, &ascii.DefaultOptions))
+	asciiString := converter.Image2ASCIIString(image, &ascii.DefaultOptions)
+	return app{asciiString: asciiString}
+}
+
+type app struct {
+	asciiString string
+}
+
+func (a app) Init() tea.Cmd {
+	return nil
+}
+
+func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		if keyMsg.Type == tea.KeyCtrlC {
+			return a, tea.Quit
+		}
+	}
+	return a, nil
+}
+
+func (a app) View() string {
+	return a.asciiString
+}
+
+func main() {
+	app := newApp()
+	prog := tea.NewProgram(app, tea.WithAltScreen())
+	_, err := prog.Run()
+	if err != nil {
+		panic(err)
+	}
 }
