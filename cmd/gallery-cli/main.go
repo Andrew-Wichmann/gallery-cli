e472494294115f18cdd44281339b1f9e6e02f643
Andrew Wichmann <andrewp.wichmann@gmail.com>
2025-04-05 16:06:03 -0400

    feat: basic carousel gallery!

--- cmd/gallery-cli/main.go
+++ cmd/gallery-cli/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"image"
 	"image/png"
 	"os"
 
@@ -9,21 +11,72 @@ import (
 )
 
 func newApp() app {
-	f, err := os.Open("images/puppy1.png")
+	var gallery gallery
+	files, err := os.ReadDir("images")
 	if err != nil {
 		panic(err)
 	}
-	image, err := png.Decode(f)
-	if err != nil {
-		panic(err)
+	for _, file := range files {
+		if !file.IsDir() {
+			f, err := os.Open(fmt.Sprintf("%s/%s", "images", file.Name()))
+			if err != nil {
+				panic(err)
+			}
+			image, err := png.Decode(f)
+			if err != nil {
+				panic(err)
+			}
+			gallery.Insert(image)
+		}
 	}
+	return app{gallery: gallery}
+}
+
+type galleryItem struct {
+	prev        *galleryItem
+	next        *galleryItem
+	asciiString string
+}
+
+type gallery struct {
+	tail *galleryItem
+	head *galleryItem
+}
+
+func (g *gallery) Insert(image image.Image) {
 	converter := ascii.NewImageConverter()
 	asciiString := converter.Image2ASCIIString(image, &ascii.DefaultOptions)
-	return app{asciiString: asciiString}
+	gi := galleryItem{asciiString: asciiString}
+	if g.head == nil {
+		if g.tail != nil {
+			panic("Head of the gallery is nil but the tail isn't")
+		}
+		g.head = &gi
+		g.tail = &gi
+		return
+	}
+	prevHead := g.head
+	g.head = &gi
+	g.head.next = prevHead
+	prevHead.prev = g.head
+	g.head.prev = g.tail
+	g.tail.next = g.head
+}
+
+func (g gallery) View() string {
+	return g.head.asciiString
+}
+
+func (g *gallery) Next() {
+	g.head = g.head.next
+}
+
+func (g *gallery) Prev() {
+	g.head = g.head.prev
 }
 
 type app struct {
-	asciiString string
+	gallery gallery
 }
 
 func (a app) Init() tea.Cmd {
@@ -35,12 +88,18 @@ func (a app) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if keyMsg.Type == tea.KeyCtrlC {
 			return a, tea.Quit
 		}
+		if keyMsg.String() == "n" {
+			a.gallery.Next()
+		}
+		if keyMsg.String() == "p" {
+			a.gallery.Prev()
+		}
 	}
 	return a, nil
 }
 
 func (a app) View() string {
-	return a.asciiString
+	return a.gallery.View()
 }
 
 func main() {
